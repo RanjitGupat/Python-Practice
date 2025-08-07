71e50cf58e95ee0b1a7694e2e400e2509e0eb867
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-08 00:02:07 +0530

    radhey.py

---
+++ Python for Data Science/radhey.py
@@ -0,0 +1,5 @@
+print("radhey")
+print('''Explored Python as a primary tool for data science, focusing on libraries such as NumPy for numerical computing, 
+Pandas for data manipulation, Matplotlib and Seaborn for data visualization, and Scikit-learn for basic machine learning tasks. 
+Practiced real-world data analysis workflows including data cleaning, exploration, visualization, and model building. Developed 
+a strong understanding of how Python supports end-to-end data science processes.''')
