38930cc6ce53ab9a51751a7b9715d9a98d5a190e
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-11 10:20:49 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 4/Problem 1.py
@@ -0,0 +1,26 @@
+#1. Write a program to store seven fruits in a list entered by the user
+
+# Method 1
+fruits = []
+
+f1 = input("Enter your fruits name: ")
+fruits.append(f1)
+f2 = input("Enter your fruits name: ")
+fruits.append(f2)
+f3 = input("Enter your fruits name: ")
+fruits.append(f3)
+f4 = input("Enter your fruits name: ")
+fruits.append(f4)
+f5 = input("Enter your fruits name: ")
+fruits.append(f5)
+f6 = input("Enter your fruits name: ")
+fruits.append(f6)
+f7 = input("Enter your fruits name: ")
+fruits.append(f7)
+
+print(fruits)
+
+
+
+
+
