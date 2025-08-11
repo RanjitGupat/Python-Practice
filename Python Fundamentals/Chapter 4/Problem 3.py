5f26d0e85858346e9ef561d7d16ae279b7d744b4
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-11 10:21:40 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 4/Problem 3.py
@@ -0,0 +1,9 @@
+# 3. Check that a tuple type cannot be changed in python.
+# Create a tuple
+my_tuple = (10, 20, 30)
+
+print("Original tuple:", my_tuple)
+# Try to change an element
+my_tuple[1] = 99  # This will cause an error
+
+#'tuple' object does not support item assignment
