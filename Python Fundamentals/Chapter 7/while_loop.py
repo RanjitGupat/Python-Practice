4580f068a63a4987fb6c557d396f403bd1425beb
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-15 12:24:55 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 7/while_loop.py
@@ -0,0 +1,24 @@
+i = 1 
+while(i < 6):
+    print(i)
+    i += 1
+
+
+# Quick Quiz: Write a program to print 1 to 50 using a while loop.
+
+j = 1 
+while(j < 51):
+    print(j)
+    j += 1
+
+
+# Quick Quiz: Write a program to print the content of a list using while loops.
+
+
+list = [1, "ranjit", "shobhit", 4, "raju"]
+
+k = 0
+
+while(k < len(list)):
+    print(list[k])
+    k += 1
