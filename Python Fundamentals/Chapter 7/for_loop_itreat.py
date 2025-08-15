2a7eb5c578f399ed2643ba793b702e80b8da848f
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-15 12:24:29 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 7/for_loop_itreat.py
@@ -0,0 +1,15 @@
+#for loop with lists
+# i = [ 1, 3, 5, 6, 7, 8, 9]
+
+# for k in i:
+#     print(k)
+
+# for loop with tuples
+# t = ( 4, 5, 6, 7)
+# for i in t:
+#     print(i)
+
+# for loop with strings
+s = "Ranjit"
+for i in s:
+    print(i)
