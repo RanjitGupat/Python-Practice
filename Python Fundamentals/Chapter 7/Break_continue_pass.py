a806335ebb2b514846f190ebb42b222acb0dc54f
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-15 12:23:57 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 7/Break_continue_pass.py
@@ -0,0 +1,78 @@
+# for i in range(100):
+#     if(i == 34):
+#         break # exit the loop right now
+#     print(i)
+
+
+""" 0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+31
+32
+33
+"""
+
+
+# for i in range(20):
+#     if(i == 14):
+#         continue # skip this iteration
+#     print(i)
+"""
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+15
+16
+17
+18
+19
+"""
+
+
+for i in range(40):
+    pass   # pass is a null statement in python. 
+           # It instructs to “do nothing”.
+
+i=0 
+while(i < 6):
+    print(i)
+    i += 1
+
---
+++ Python Fundamentals/Chapter 7/for_loop.py
@@ -0,0 +1,5 @@
+# for i in range(6):
+#     print(i)
+
+for ik in range(1,10,2): #start, stop, step size
+    print(ik)            #step size
