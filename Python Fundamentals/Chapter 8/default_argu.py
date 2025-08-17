573a51995876d724f61a77c8602c65146027af59
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-17 10:32:20 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 8/default_argu.py
@@ -0,0 +1,10 @@
+def goodDay(name, ending="Thank You"):
+    print("Good Day, "+ name)
+    print(ending)
+    return "done"
+
+a = goodDay("Ranjit")
+print(a)
+
+a = goodDay("Raj","Thanks")
+print(a)
