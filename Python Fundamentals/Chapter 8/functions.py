08c5b679f816c8b750fbc294a9716cd93a97fdd0
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-17 10:32:49 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 8/functions.py
@@ -0,0 +1,26 @@
+# a = int(input("enter a number : "))
+# b = int(input("enter a number : "))
+# c = int(input("enter a number : "))
+# average = (a + b + c) / 3
+# print(average)
+
+# a = int(input("enter a number : "))
+# b = int(input("enter a number : "))
+# c = int(input("enter a number : "))
+# average = (a + b + c) / 3
+# print(average)
+
+
+def avg():            #function definition
+    a = int(input("enter a number : "))
+    b = int(input("enter a number : "))
+    c = int(input("enter a number : "))
+    average = (a + b + c) / 3
+    print(average)
+
+avg()                #function call
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
