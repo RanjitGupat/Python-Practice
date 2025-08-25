73323b4807b907d6340bb9b6a9beda6f131fa79e
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-25 20:27:46 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 9/Problem 1.py
@@ -0,0 +1,12 @@
+# 1. Write a program to read the text from a given file ‘poems.txt’ and find out
+# whether it contains the word ‘twinkle’.
+
+
+f = open("poem.txt")
+content = f.read()
+if("twinkle" in content):
+    print("the word twinkle is present in the content")
+else:
+    print("the word twinkle is not present in the content")
+
+f.close()
