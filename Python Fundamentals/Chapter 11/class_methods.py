b08dc830829e88e8efd0ec5b83d161f67e06c593
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-08-28 23:42:03 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 11/class_methods.py
@@ -0,0 +1,14 @@
+# CLASS METHOD
+# A class method is a method which is bound to the class and not the object of the class.
+# @classmethod decorator is used to create a class method.
+
+
+class Employee:
+    a = 1
+    @classmethod
+    def show(self):
+        print(f"The class value of a is {self.a}")
+
+e = Employee()
+e.a = 45
+e.show()
