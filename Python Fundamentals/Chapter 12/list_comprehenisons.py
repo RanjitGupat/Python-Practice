ae967837a84be5bc7fa6c6df8c20ab4d33f15c05
Ranjit Gupta <155065241+RanjitGupat@users.noreply.github.com>
2025-09-02 10:21:05 +0530

    Add files via upload

---
+++ Python Fundamentals/Chapter 12/list_comprehenisons.py
@@ -0,0 +1,12 @@
+myList = [1, 2, 9, 5, 6, 7, 8]
+
+# squaredLst = []
+# for item in myList:
+#     squaredLst.append(item*item)
+
+# print(squaredLst)
+
+
+squaredlist = [i*i for i in myList]
+
+print(squaredlist)
